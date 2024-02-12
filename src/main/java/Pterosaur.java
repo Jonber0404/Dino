84e56b8575a1287a4ef15fd67ee6637f5d3bf057
sigzal1105 <sigzal1105@skola.goteborg.se>
2024-02-12 12:07:00 +0100

    Added wingspan, altitude, speed variables, and getters and setters for altitude and speed. Added constructor. Added fly() and land() methods

--- src/main/java/Pterosaur.java
+++ src/main/java/Pterosaur.java
@@ -1,3 +1,54 @@
-public class Pterosaur  {
+public class Pterosaur extends Dinosaur {
+    private final double wingspan; // meters
+    private double speed = 0.0; // km/h
+    private double altitude = 0.0; // meters
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public Pterosaur(boolean herbivore, boolean carnivore, boolean omnivore, double wingspan) {
+        super(herbivore, carnivore, omnivore);
+        this.wingspan = wingspan;
+        if (herbivore) {
+            throw new IllegalArgumentException("Pterosaurs cannot be herbivores");
+        }
+    }
+
+    /**
+     * Simulates flight. Changes the altitude of
+     * the Pterosaur. Flying drains energy. If
+     * energy is zero or below you cannot fly.
+     */
+    public void fly() {
+        if (getEnergy() > 0) {
+            if (getEnergy() >= 50 && getEnergy() <= 100) {
+                speed = speed + 24;
+            } else if (getEnergy() > 0 && getEnergy() < 50) {
+                speed = speed - 24;
+            }
+
+            altitude = altitude + speed;
+            setEnergy(getEnergy() - 10);
+        } else {
+            if (altitude > 0) {
+                land();
+            }
+        }
+    }
+
+    /**
+     * Simulates landing.
+     */
+    public void land() {
+        if (altitude > 0) {
+            altitude = 0;
+            speed = 0;
+        }
+    }
 
 }
